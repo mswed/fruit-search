3dbff5c6eb7f2f55d7be46f4b737d32cb817247a
mswed <mswed@beapot.com>
2023-10-03 18:53:18 -0400

    basic functionallity done (with css)

--- script.js
+++ script.js
@@ -4,27 +4,48 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
+	// Convert search string to lowercase
 	const lowerCaseSearch = str.toLowerCase()
+
 	let results =  fruit.filter((item) => {
+		// Convert fruit item to lowercase
 		const lowerCaseItem = item.toLowerCase();
-
-		return lowerCaseItem.indexOf(lowerCaseSearch) !== -1;
+		// Add fruit to result if the search string is found
+		return lowerCaseItem.indexOf(lowerCaseSearch) !== -1 && lowerCaseSearch !== '';
 	});
-
+	// console.log('new results', results)
 	return results;
 }
 
 function searchHandler(e) {
-	search(input.value)
+	console.log('Calling searchHandler()')
+	// Filter the fruit list
+	let results = search(input.value)
+	// Add the results to the dropdown
+	showSuggestions(results, input.value)
+
+
 }
 
 function showSuggestions(results, inputVal) {
-
-	// TODO
+	// Clear the list
+	suggestions.innerHTML = '';
+	for (let fruit of results) {
+		// Create list item
+		const li = document.createElement('li');
+		li.innerText = fruit;
+		suggestions.append(li)
+	}
+	if (suggestions.childElementCount > 0) {
+		suggestions.classList.add('has-suggestions')
+	} else {
+		suggestions.classList.remove('has-suggestions')
+	}
 }
 
 function useSuggestion(e) {
-	// TODO
+	input.value = e.target.innerText;
+	suggestions.innerHTML = '';
 }
 
 input.addEventListener('keyup', searchHandler);
--- script.test.js
+++ script.test.js
@@ -8,4 +8,20 @@ describe('Tests for search()', () => {
     it('should should return an empty array if no items are found', () => {
         expect(search('tom sawyer ')).toEqual([])
     });
+
 });
+
+describe('Tests for showSuggestions()', () => {
+    it('should add the found items to the suggestions div', () => {
+        showSuggestions(['Apple', 'Custard apple', 'Pineapple']);
+        expect(suggestions.childElementCount).toEqual(3);
+        expect(suggestions.children[0].innerText).toEqual('Apple');
+        expect(suggestions.children[1].innerText).toEqual('Custard apple');
+        expect(suggestions.children[2].innerText).toEqual('Pineapple');
+    });
+});
+
+afterEach(() => {
+    suggestions.innerHTML = '';
+    suggestions.classList.remove('has-suggestions')
+})
